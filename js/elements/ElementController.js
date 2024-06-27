c0e19b7d0dece9b095b35431e7b123637cb42161
moSa963 <mo.sa963@hotmail.com>
2024-06-27 20:44:32 +0300

    Fix ElementController has no 'left' prop

--- js/elements/ElementController.js
+++ js/elements/ElementController.js
@@ -4,33 +4,34 @@
  * Class used to control an html element to do basic operations like scale and move.
  * @param {HTMLElement} element 
  */
-class ElementController{
+class ElementController {
     /** 
     * @param {HTMLElement} element 
     */
-    constructor(element){
+    constructor(element) {
         this.element = element;
         this.top = 0;
+        this.left = 0;
         this.startPoint = null;
         this.initWidth = element.clientWidth;
-        this.width =  element.clientWidth;
+        this.width = element.clientWidth;
         this.scaleRatio = element.clientWidth / element.clientHeight;
     }
 
-    onClick(event){
+    onClick(event) {
         this.startPoint = new Point(event.clientX, event.clientY);
     }
 
-    onHold(event){
-        if (this.startPoint != null){
-            this.move(event.clientX - this.startPoint.x , event.clientY - this.startPoint.y );
+    onHold(event) {
+        if (this.startPoint != null) {
+            this.move(event.clientX - this.startPoint.x, event.clientY - this.startPoint.y);
         }
     }
 
-    onRelease(event){
-        if (this.startPoint != null){
-            var left = event.clientX - this.startPoint.x + this.left;
-            var top = event.clientY - this.startPoint.y + this.top;
+    onRelease(e) {
+        if (this.startPoint != null) {
+            var left = e.clientX - this.startPoint.x + this.left;
+            var top = e.clientY - this.startPoint.y + this.top;
             this.element.style.left = left + "px";
             this.element.style.top = top + "px";
             this.left = left;
@@ -39,33 +40,33 @@ class ElementController{
         }
     }
 
-    scale(count){
+    scale(count, min = 200, max = 3000) {
         this.width += count;
 
-        if (this.width < 200){
-            this.width = 200;
-        }else if (this.width > 3000){
-            this.width = 3000;
+        if (this.width < min) {
+            this.width = min;
+        } else if (this.width > max) {
+            this.width = max;
         }
 
         this.element.style.width = this.width + 'px';
         this.element.style.height = (Math.floor(this.width / this.scaleRatio)) + 'px';
     }
 
-    move(countX, countY){
-        this.element.style.left = (countX + this.left) + "px";
-        this.element.style.top = (countY + this.top) + "px";
+    move(x, y) {
+        this.element.style.left = (x + this.left) + "px";
+        this.element.style.top = (y + this.top) + "px";
     }
 
-    getScaleRatio(){
+    getScaleRatio() {
         return this.width / this.initWidth;
     }
 
-    getWidth(){
+    getWidth() {
         return this.width;
     }
 
-    getHeight(){
+    getHeight() {
         return Math.floor(this.width / this.scaleRatio);
     }
 }
