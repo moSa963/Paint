2a05d12c1dc657ecfe8fd2709c6f5999e49d957f
moSa963 <mo.sa963@hotmail.com>
2024-06-30 18:11:36 +0300

    Refactor Canvas

--- js/elements/Canvas.js
+++ js/elements/Canvas.js
@@ -1,45 +1,44 @@
 
-class Canvas{
+class Canvas {
     /** 
     * @param {HTMLCanvasElement} canvas 
     */
-    constructor(canvas){
+    constructor(canvas) {
         this.canvas = canvas;
         this.setData("transparent", 10);
     }
 
-    setData(color, size){
+    setData(color, size) {
         var context = this.canvas.getContext("2d");
         context.lineWidth = size;
-        
-        if (color === 'none'){
-            context.globalCompositeOperation = 'destination-out';
-
-        } else {
-            context.globalCompositeOperation = 'source-over';
-            context.strokeStyle = color;
-            context.fillStyle = color;
-        }
-
         context.lineCap = 'round';
         context.lineJoin = 'round';
+
+        if (color === 'none') {
+            context.globalCompositeOperation = 'destination-out';
+            return;
+        }
+
+        context.globalCompositeOperation = 'source-over';
+        context.strokeStyle = color;
+        context.fillStyle = color;
     }
 
-    drawEllipse(point, r1, r2){
+    drawEllipse(point, r1, r2) {
         var context = this.canvas.getContext("2d");
         context.beginPath();
         context.ellipse(point.x, point.y, Math.abs(r1), Math.abs(r2), Math.PI * 2, 0, Math.PI * 2);
         context.stroke();
     }
 
-    drawRect(point1, point2){
+    drawRect(point1, point2) {
         var context = this.canvas.getContext("2d");
         context.beginPath();
         context.rect(point1.x, point1.y, point2.x - point1.x, point2.y - point1.y);
         context.stroke();
     }
 
-    drawLine(point1, point2){
+    drawLine(point1, point2) {
         var context = this.canvas.getContext("2d");
         context.beginPath();
         context.moveTo(point1.x, point1.y);
@@ -47,7 +46,7 @@ class Canvas{
         context.stroke();
     }
 
-    drawTriangle(point1, point2){
+    drawTriangle(point1, point2) {
         var context = this.canvas.getContext("2d");
         context.beginPath();
         context.moveTo(point1.x, point2.y);
@@ -57,41 +56,41 @@ class Canvas{
         context.stroke();
     }
 
-    drawImage(source, x, y){
+    drawImage(source, x, y) {
         var context = this.canvas.getContext("2d");
         context.drawImage(source, x, y);
     }
-    
-    newBackup(){
-        var backup = document.createElement('canvas');
+
+    newBackup() {
+        const backup = document.createElement('canvas');
         backup.width = this.canvas.width;
         backup.height = this.canvas.height;
-        var context = backup.getContext("2d");
+        const context = backup.getContext("2d");
         context.drawImage(this.canvas, 0, 0);
         return backup;
     }
 
-    getImageData(willReadFrequently){
-        const ctx = this.canvas.getContext("2d",  {
+    getImageData(willReadFrequently) {
+        const ctx = this.canvas.getContext("2d", {
             willReadFrequently: Boolean(willReadFrequently)
         })
-        
+
         return ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
     }
 
-    getWidth(){
+    getWidth() {
         return this.canvas.width;
     }
 
-    getHeight(){
+    getHeight() {
         return this.canvas.height;
     }
 
-    putImageData(imgData){
+    putImageData(imgData) {
         this.canvas.getContext("2d").putImageData(imgData, 0, 0);
     }
 
-    clear(){
+    clear() {
         var context = this.canvas.getContext("2d");
         context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
