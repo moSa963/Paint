2eaf4aff6891b09b5decfdea94b7cc31bb0e4b84
moSa963 <mo.sa963@hotmail.com>
2024-07-02 21:13:10 +0300

    Add ToolsBar class

---
+++ js/elements/ToolsBar.js
@@ -0,0 +1,77 @@
+
+
+
+class ToolsBar {
+    constructor() {
+        this.container = document.getElementById('toolsBar');
+        const toolsContainer = document.createElement('ul');
+        this.selectedTool = null;
+        this.onChange = null;
+
+        for (const key of Object.keys(tools)) {
+            const tool = document.createElement('li');
+            tool.classList.add('clickable');
+            if (key == "select") {
+                tool.classList.add('selected');
+                this.selectedTool = {
+                    tool: tools[key],
+                    element: tool,
+                };
+            }
+            tool.title = key;
+            tool.onclick = () => {
+                this.handleToolSelected(tool, key);
+            }
+            tool.appendChild(getIcon(tools[key].icon));
+
+            toolsContainer.appendChild(tool);
+        }
+
+        toolsContainer.appendChild(document.createElement('hr'));
+
+        const tool = document.createElement('li');
+        tool.classList.add('clickable');
+        tool.title = "theme";
+
+        tool.onclick = () => {
+            this.themeToggle(tool);
+        }
+        tool.appendChild(getIcon('lightIcon.svg'));
+        toolsContainer.appendChild(tool);
+        this.container.appendChild(toolsContainer);
+    }
+
+    handleToolSelected(element, name) {
+        this.selectedTool.element.classList.remove('selected');
+        element.classList.add('selected');
+        this.selectedTool = {
+            tool: tools[name],
+            element,
+        };
+
+        this.onChange && this.onChange(this.getSelectedTool())
+    }
+
+    themeToggle(tool) {
+        const body = document.querySelector('body');
+        if (body.classList.contains('dark')) {
+            body.classList.remove('dark');
+
+            tool.innerHTML = "";
+            tool.appendChild(getIcon('darkIcon.svg'));
+        } else {
+            body.classList.add('dark');
+            tool.innerHTML = "";
+            tool.appendChild(getIcon('lightIcon.svg'));
+        }
+    }
+
+    getSelectedTool() {
+        return this.selectedTool.tool;
+    }
+
+    toggleVisibility() {
+        this.container.style.display = this.container.style.display == 'none' ? "flex" : 'none';
+    }
+}
+
