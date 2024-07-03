55ef028f8766e7def32da6038247fd9dde63635d
moSa963 <mo.sa963@hotmail.com>
2024-07-03 22:07:11 +0300

    Remove unused functions 'compareColors' and 'indexToPoint

--- js/utility.js
+++ js/utility.js
@@ -12,11 +12,3 @@ const getIcon = (name) => {
     icon.style.backgroundImage = `url(./res/${name}Icon.svg)`;
     return icon;
 }
-
-const indexToPoint = (index, width) => {
-    return new Point(index % width, parseInt(index / width));
-}
-
-const compareColors = (c1, c2) => {
-    return c1[0] === c2[0] && c1[1] === c2[1] && c1[2] === c2[2] && c1[3] === c2[3];
-}
